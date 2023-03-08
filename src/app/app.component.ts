b00c903537f98690eadacabe4be8d83c7b111e8e
Tomasz Flis <tomaszwwl@gmail.com>
2023-03-08 21:32:04 +0100

    feat(app): register view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'my-estate-webapp-root',
-  template: `Hello`,
-  styleUrls: ['./app.component.scss'],
+  template: ` <router-outlet />`,
   standalone: true,
+  imports: [RouterOutlet],
 })
 export default class AppComponent {}
